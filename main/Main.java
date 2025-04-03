84063edd336e011ea19ea5f4399c21944d6c4f33
charanramagiri <154454369+charanramagiri@users.noreply.github.com>
2025-04-03 14:05:12 +0530

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -3,6 +3,27 @@ package main;
 public class Main {
     public static int longestMountain(int[] arr) {
         // code for Longest Mountain
+        if(arr.length<3) return 0;
+
+        int max =0;
+        int n = arr.length;
+
+        for(int i=1;i<n-1;i++){
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                int left=i,right=i;
+
+                while(left>0 && arr[left]> arr[left-1]){
+                    left--;
+                }
+                while(right<n-1 && arr[right]> arr[right+1]){
+                    right++;
+                }
+
+                int length = right-left+1;
+                max=Math.max(max,length);
+            }
+        }
+        return max;
     }
 
 }
